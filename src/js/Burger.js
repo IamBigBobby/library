a2412bba3b41a1ce4c64a08f4609b58b5e571906
Denis Svetleishii <denissvetleishii@outlook.com>
2023-10-19 19:41:06 +0400

    feat: add opacity background and unscroll

--- src/js/Burger.js
+++ src/js/Burger.js
@@ -5,6 +5,12 @@ export function toggleBurger(){
     console.log(event.target);
     burger.classList.toggle('burger-menu_rotate');
     navigation.classList.toggle('navigation__list_visible-tablet');
+
+    if (document.body.style.overflow === "hidden"){
+      document.body.style.overflow = "";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
   })
 }
 toggleBurger();
