7841002c532917a8f8cad82acafc13d78e231715
Denis Svetleishii <denissvetleishii@outlook.com>
2023-10-31 17:13:14 +0400

    feat: add complete burger menu

--- src/js/Burger.js
+++ src/js/Burger.js
@@ -1,6 +1,7 @@
 export function toggleBurger(){
   let burger = document.querySelector('.burger-menu');
   let navigation = document.querySelector('.navigation__list');
+  let navigationLink = document.querySelectorAll('.navigation__link')
   burger.addEventListener('click', (event) => {
     burger.classList.toggle('burger-menu_rotate');
     navigation.classList.toggle('navigation__list_visible-tablet');
@@ -10,6 +11,15 @@ export function toggleBurger(){
     } else {
       document.body.style.overflow = "hidden";
     }
-  })
+  });
+  navigationLink.forEach((element) => {
+    element.addEventListener('click', () => {
+    navigation.classList.remove('navigation__list_visible-tablet');
+    burger.classList.toggle('burger-menu_rotate');
+    document.body.style.overflow = "";
+    });
+  });
+
+  
 }
 toggleBurger();
